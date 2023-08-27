54f233ce015a6e60a4798ae5e4f4134f0d3a9121
Diego Oliveira <diego.oliveira@grupocheck.com.br>
2023-08-27 18:18:26 -0300

    função de conclusão de tarefas e contador de tarefas concluídas

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ btnAdd.addEventListener('click', function(e){
 
     listarTarefas();
     limparInput();
+    total();
 })
 
 const limparInput = () => descricao.value = '';
@@ -23,11 +24,22 @@ const listarTarefas = () => {
     tarefas.appendChild(li);
 
     lista.push(descricao.value);
-
-    if(lista.length < 2){
-        quantidade.innerText = `Você tem ${lista.length} tarefa`;
-    } else {
-        quantidade.innerText = `Você tem ${lista.length} tarefas`;
+    
+    li.onclick = function(e){
+        e.preventDefault();
+        concluida(this);
     }
+}
+
+const total = () => {
+    lista.length > 1 ? quantidade.innerText = `Você tem ${lista.length} tarefas` : quantidade.innerText = `Você tem ${lista.length} tarefa`;
+}
+
+const concluida = (li) => {
+    li.classList.toggle('concluida');
+
+    const qtdDoncluidas = document.querySelectorAll('.concluida').length;
+
+    qtdDoncluidas > 1 ? document.getElementById('concluidas').innerText = ` e ${qtdDoncluidas} concluídas` : document.getElementById('concluidas').innerText = ` e ${qtdDoncluidas} concluída`;
 
 }
